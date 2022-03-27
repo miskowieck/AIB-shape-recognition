55d2ccd0f2c5966c914e857fd1762a3b2bbcc3a5
Luki <hexeyxd@gmail.com>
2022-03-27 19:56:39 +0200

    DUPA

---
+++ main.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt # for plotting
+import numpy as np # for transformation
+
+import torch # PyTorch package
+import torchvision # load datasets
+import torchvision.transforms as transforms # transform data
+import torch.nn as nn # basic building block for neural neteorks
+import torch.nn.functional as F # import convolution functions like Relu
+import torch.optim as optim # optimzer
+# python image library of range [0, 1]
+# transform them to tensors of normalized range[-1, 1]
+
+transform = transforms.Compose( # composing several transforms together
+    [transforms.ToTensor(), # to tensor object
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # mean = 0.5, std = 0.5
+
+# set batch_size
+batch_size = 4
+
+# set number of workers
+num_workers = 2
+
+# load train data
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=num_workers)
+
+# load test data
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=num_workers)
+
+# put 10 classes into a set
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
